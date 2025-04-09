b34f10a0e605422323d2ac3eed5e107358512289
jienzheng <jienzheng02@gmail.com>
2025-04-09 16:03:17 -0700

    New CRUD added for match

--- backend/src/app/services/match.py
+++ backend/src/app/services/match.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+import uuid
 
 from app.core.config import get_settings
 from app.models.match import Match
@@ -8,7 +9,19 @@ settings = get_settings()
 
 # MATCH CRUD
 
-def create_match(db: Session, match: MatchHistory):
+def start_match(db: Session, match: MatchHistory):
+    new_match = Match(
+        match_id=str(uuid.uuid4()),
+        user_id=match.hostID,
+        opponent_id=match.guestID,
+        problem_id=match.problemID
+    )
+    db.add(new_match)
+    db.commit()
+    db.refresh(new_match)
+    return new_match
+
+def end_match(db: Session, match: MatchHistory):
     db_match = Match(
         match_id=match.matchID,
         user_id=match.hostID,
@@ -19,3 +32,9 @@ def create_match(db: Session, match: MatchHistory):
     db.commit()
     db.refresh(db_match)
     return db_match
+
+def get_match(db: Session, matchID: str):
+    return db.query(Match).filter(Match.match_id == matchID).first()
+
+def get_all_matches(db: Session):
+    return db.query(Match).all()
