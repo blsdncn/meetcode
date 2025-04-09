443de1c68070bf4c9359560ff58cd3f1572852b1
jienzheng <jienzheng02@gmail.com>
2025-04-08 18:15:30 -0700

    fixed up the model, added orm for some fields

--- backend/src/app/models/match.py
+++ backend/src/app/models/match.py
@@ -5,11 +5,17 @@ from app.core.database import Base
 class MatchHistory(Base):
     __tablename__ = "matchHistory"
     
-    matchID: UUID = Column(String, primary_key=True, index=True)
+    matchID: UUID = Column(String, primary_key=True, unique=True, index=True)
     hostID: UUID = Column(String, ForeignKey("users.id"), index=True)
     guestID: UUID = Column(String, ForeignKey("users.id"), index=True)
+    
     startTime: DateTime = Column(DateTime, default=datetime.now(UTC))
     endTime: DateTime = Column(DateTime, default=datetime.now(UTC))
     status: Boolean = Column(Boolean, default=True)
     duration: Integer = Column(Integer, default=0)
     
+    problemID = Column(Integer, ForeignKey("problems.id"), index=True)
+    
+    host = relationship("User", foreign_keys=[hostID])
+    guest = relationship("User", foreign_keys=[guestID])
+    problem = relationship("Problem", back_populates="matches", foreign_keys=[problemID])
