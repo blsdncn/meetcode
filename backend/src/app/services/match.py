27808b097fee57e23927feff1229a77ad3bb83c7
jienzheng <jienzheng02@gmail.com>
2025-04-09 14:59:49 -0700

    Renamed variables for match db

--- backend/src/app/services/match.py
+++ backend/src/app/services/match.py
@@ -1,23 +1,21 @@
 from sqlalchemy.orm import Session
 
-from app.core.auth import get_password_hash
 from app.core.config import get_settings
-#from app.core.security import verify_password
 from app.models.match import Match
 from app.schemas.match import MatchHistory
 
 settings = get_settings()
 
-# USER CRUD
+# MATCH CRUD
 
 def create_match(db: Session, match: MatchHistory):
-    new_match = Match(
+    db_match = Match(
         match_id=match.matchID,
         user_id=match.hostID,
         opponent_id=match.guestID,
         problem_id=match.problemID
     )
-    db.add(new_match)
+    db.add(db_match)
     db.commit()
-    db.refresh(new_match)
-    return new_match
+    db.refresh(db_match)
+    return db_match
