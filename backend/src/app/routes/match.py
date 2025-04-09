9a13743e3c8a573a4ca9766041e01cb64d179c6a
jienzheng <jienzheng02@gmail.com>
2025-04-09 15:00:34 -0700

    Added match as a db service

--- backend/src/app/routes/match.py
+++ backend/src/app/routes/match.py
@@ -1,7 +1,7 @@
 from app.schemas.match import MatchDetails, MatchUpdate, MatchHistoryUpdate, MatchStart, MatchEnd, MatchHistory
 from fastapi import APIRouter, Depends
 
-import app.services.user as user_service
+import app.services.match as match_service
 from app.dependencies import get_db
 from sqlalchemy.orm import Session
 
@@ -9,10 +9,10 @@ router = APIRouter()
 
 @router.post("/match/start", response_model=MatchStart, tags=["Match"])
 def start_match(match: MatchStart, db: Session = Depends(get_db)):
-    new_match = user_service.start_match(db=db, match=match)
+    new_match = match_service.start_match(db=db, match=match)
     return new_match
 
 @router.post("/match/end/{matchID}", response_model=MatchEnd, tags=["Match"])
 def end_match(match: MatchEnd, db: Session = Depends(get_db)):
-    match_over = user_service.end_match(db=db, match=match)
+    match_over = match_service.end_match(db=db, match=match)
     return match_over
