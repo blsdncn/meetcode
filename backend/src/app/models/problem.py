8b54512438ce732b41bd3d4a8531fb347b2fb3cc
jienzheng <jienzheng02@gmail.com>
2025-04-09 15:01:28 -0700

    Created a mock problem.py file

---
+++ backend/src/app/models/problem.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+from uuid import UUID
+from sqlalchemy.orm import relationship
+
+class Problem(Base):
+    __tablename__ = "problems"
+
+    id = Column(Integer, primary_key=True, index=True)
+    problem_link = Column(String, index=True)
+    method_link = Column(String, index=True)
+    category = Column(String, index=True)
+
+    matches = relationship("Match", back_populates="problem", foreign_keys="[Match.problemID]")
