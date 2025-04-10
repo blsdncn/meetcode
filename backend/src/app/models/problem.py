5ec1902cda184a10dab9eb09df3cf0360500afda
oyasumiku <98620862+oyasumiku@users.noreply.github.com>
2025-04-10 16:29:48 -0700

    added attributes

--- backend/src/app/models/problem.py
+++ backend/src/app/models/problem.py
@@ -1,3 +1,14 @@
 from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from datetime import UTC, datetime
 from app.core.database import Base
+
+class Problem(Base):
+    __tablename__ = "problems"
+
+    ## ATTRIBUTES ## 
+    lc_id = Column(Integer, primary_key=True, index=True)
+    problem_link = Column(String)
+    methods_video_link = Column(String)
+    category = Column(String)
+    
+    ## RELATIONSHIPS ##
