829b1642ba821c31d3bdf8c0fcd0a84cb9bc0f2f
blsdncn <bduncan3915@sdsu.edu>
2025-04-10 16:49:17 -0700

    created route to get to websocket-testing page

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from app.core.database import Base, engine
 from app.routes.user import router as user_router
+from app.routes.websocket import router as websocket_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user_router, prefix="/auth", tags=["User"])
+app.include_router(websocket_router, prefix="/ws", tags=["WebSocket"])
 
 
 @app.get("/")
---
+++ backend/src/app/routes/websocket.py
@@ -0,0 +1,57 @@
+from fastapi.responses import HTMLResponse
+from app.schemas.user import UserResponse, UserCreate
+from fastapi import APIRouter, Depends
+
+import app.services.user as user_service
+from app.dependencies import get_db
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+        var ws = null;
+            function connect(event) {
+                var itemId = document.getElementById("itemId")
+                var token = document.getElementById("token")
+                ws = new WebSocket("ws://localhost:8000/items/" + itemId.value + "/ws?token=" + token.value);
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                event.preventDefault()
+            }
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+@router.get("/")
+async def websocket_endpoint():
+    return HTMLResponse(html)
