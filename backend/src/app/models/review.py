437d8b76633fea06475a6ca1a9e99d8cb7434957
teetruc <ttruc7290@sdsu.edu>
2025-04-11 12:11:00 -0700

    Added user relationships to the review model

--- backend/src/app/models/review.py
+++ backend/src/app/models/review.py
@@ -1,6 +1,7 @@
 from datetime import UTC, datetime
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Interval, ForeignKey
+from sqlalchemy.orm import relationship
 from uuid import UUID
 from app.core.database import Base
 
@@ -25,3 +26,6 @@ class Rating(Base):
 
     host_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     guest_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    host = relationship("User", foreign_keys=[host_id])
+    guest = relationship("User", foreign_keys=[guest_id])
