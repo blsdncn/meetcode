727a0f76610c02140788625213fbb84c2b0cc4af
jienzheng <jienzheng02@gmail.com>
2025-04-11 12:33:58 -0700

    Reverted changes to main

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -5,9 +5,7 @@ from app.routes import api_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-# app.include_router(api_router, prefix="")
-app.include_router(user_router, prefix="/auth", tags=["User"])
-app.include_router(match_router, prefix="/api", tags=["Match"])
+app.include_router(api_router, prefix="")
 
 
 @app.get("/")
