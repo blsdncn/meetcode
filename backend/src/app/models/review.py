8b5ae76e6c9f69cf775127580d4118867571ed64
teetruc <ttruc7290@sdsu.edu>
2025-04-10 22:12:21 -0700

    Adding review schema and altering tab spacing for review model file

--- backend/src/app/models/review.py
+++ backend/src/app/models/review.py
@@ -5,24 +5,23 @@ from uuid import UUID
 from app.core.database import Base
 
 class Rating(Base):
-  __tablename__ = 'reviews'
+    __tablename__ = 'reviews'
 
-  id = Column(Integer, primary_key = True, index = True)
+    id = Column(Integer, primary_key = True, index = True)
 
-  match_id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    match_id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
 
-  to_host_rating = Column(Integer, nullable=False)
-  to_guest_rating = Column(Integer, nullable=False)
+    to_host_rating = Column(Integer, nullable=False)
+    to_guest_rating = Column(Integer, nullable=False)
 
-  to_host_comment = Column(String, nullable=True)
-  to_guest_comment = Column(String, nullable=True)
+    to_host_comment = Column(String, nullable=True)
+    to_guest_comment = Column(String, nullable=True)
 
-  problem_solved = Column(Boolean, nullable=False)
-  time_given = Column(Integer, nullable=False)
+    problem_solved = Column(Boolean, nullable=False)
+    time_given = Column(Integer, nullable=False)
 
-  elapsed_time = Column(Interval, nullable=False)
-  review_updated_at = Column(DateTime, default=lambda:datetime.now(UTC), onupdate=lambda:datetime.now(UTC))
+    elapsed_time = Column(Interval, nullable=False)
+    review_updated_at = Column(DateTime, default=lambda:datetime.now(UTC), onupdate=lambda:datetime.now(UTC))
 
-  host_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-  guest_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-
+    host_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    guest_id = Column(Integer, ForeignKey("users.id"), nullable=False)
