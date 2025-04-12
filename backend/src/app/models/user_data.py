4b5dffcb3ed048d381eaaf50ff726d83e4c60904
blsdncn <bduncan3915@sdsu.edu>
2025-04-12 00:27:33 -0700

    changed userid from int to UUID

--- backend/src/app/models/user_data.py
+++ backend/src/app/models/user_data.py
@@ -8,7 +8,7 @@ class UserData(Base):
     __tablename__ = "user_data"
 
     id = Column(Integer, primary_key = True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
+    user_id = Column(String, ForeignKey("users.id"), unique=True, nullable=False)
 
     profile_picutre = Column(String, nullable=True)
     last_login = Column(DateTime, default=datetime.now(UTC))
