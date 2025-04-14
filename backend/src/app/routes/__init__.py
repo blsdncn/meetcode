c518e15b4a159d13053cf4e9ec0d0785d80dc3c4
jienzheng <jienzheng02@gmail.com>
2025-04-13 21:16:10 -0700

    Renamed the api endpoint names

--- backend/src/app/routes/__init__.py
+++ backend/src/app/routes/__init__.py
@@ -5,6 +5,6 @@ from app.routes import user, user_data, match, review, problem
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/auth", tags=["User"])
 api_router.include_router(user_data.router, prefix="/data", tags=["User Data"])
-api_router.include_router(match.router, prefix="/api", tags=["Match"])
+api_router.include_router(match.router, prefix="/api/match", tags=["Match"])
 api_router.include_router(review.router, prefix="/api/reviews", tags=["Reviews"])
 api_router.include_router(problem.router, prefix="/api/problem", tags=["Problem"])
