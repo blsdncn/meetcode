8be3b697bb62b6bfe5c12197484eacdc0839888b
blsdncn <bduncan3915@sdsu.edu>
2025-04-13 20:17:22 -0700

    fixed user model to say Review, crud for problem

--- backend/src/app/models/problem.py
+++ backend/src/app/models/problem.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import JSON, Boolean, Column, DateTime, Integer, String
 from datetime import UTC, datetime
 from app.core.database import Base
 
@@ -6,9 +6,11 @@ class Problem(Base):
     __tablename__ = "problems"
 
     ## ATTRIBUTES ## 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    problem_id = Column(Integer, unique=True, index=True)
     problem_link = Column(String)
     methods_video_link = Column(String)
-    category = Column(String)
+    categories = Column(JSON)
     
     ## RELATIONSHIPS ##
+    
--- backend/src/app/routes/problem.py
+++ backend/src/app/routes/problem.py
@@ -1,17 +1,20 @@
 from datetime import timedelta
 from app.core.auth import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from app.schemas.problem import ProblemCreate
 from app.schemas.token import Token
 from app.schemas.user import UserResponse, UserCreate
 from fastapi import APIRouter, Depends
 
 import app.services.user as user_service
+import app.services.problem as problem_service
 from app.dependencies import get_db
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 
 #from app.schemas.problem import UserReviewCreate, UserReviewRead
-from app.models.problem import Problem
+from app.models.problem import Problem as ProblemModel
+from app.schemas.problem import Problem
 from app.models.user import User
 from app.schemas.user_match_history import UserMatchHistory
 
@@ -19,16 +22,32 @@ from app.schemas.user_match_history import UserMatchHistory
 
 router = APIRouter()
 
+@router.post("api/problems/create", response_model=Problem)
+def create_problem(problem: ProblemCreate, db: Session = Depends(get_db)):
+    new_problem = problem_service.create_problem(db=db, problem=problem)
+    return new_problem
+
+@router.get("/api/problems/{lc_id}")
+def get_single_problem(lc_id: int, db: Session = Depends(get_db)):
+    problem = problem_service.get_problem(db=db, lc_id=lc_id)
+    if not problem:
+        raise HTTPException(status_code=404, detail="Problem not found")
+    return problem
+
+@router.put("/api/problems/update/{lc_id}", response_model=Problem)
+def update_problem(lc_id: int, problem: ProblemCreate, db: Session = Depends(get_db)):
+    result = problem_service.update_problem(db=db, lc_id=lc_id, problem=problem)
+    return result
+
+@router.delete("/api/problems/delete/{lc_id}")
+def delete_problem(lc_id: int, db: Session = Depends(get_db)):
+    result = problem_service.delete_problem(db=db, lc_id=lc_id)
+    return result
+
 @router.get("/api/problems")
 def get_all_problems(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     problems = db.query(Problem).offset(skip).limit(limit).all()
     return problems
-
-@router.get("/api/problems/{lc_id}")
-def get_single_problem(lc_id: int, db: Session = Depends(get_db)):
-    problem = db.query(Problem).filter(Problem.id == lc_id)
-    
-    return problem
 
 
 # TODO: Figure out how to integrate with user match history (too difficult while they're separated)
