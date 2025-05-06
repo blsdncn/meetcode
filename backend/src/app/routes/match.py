dc3af9e55753cce0e99b963a3cb5f807edb00a6a
jienzheng <jienzheng02@gmail.com>
2025-05-05 19:43:49 -0700

    minor changes

--- backend/src/app/routes/match.py
+++ backend/src/app/routes/match.py
@@ -48,8 +48,8 @@ def get_match_details(match_id: UUID, db: Session = Depends(get_db)):
 
 
 @router.get("/history/{user_id}", response_model=list[MatchHistory], tags=["Match"])
-def get_match_history(match_id: UUID, db: Session = Depends(get_db)):
-    matches = match_service.get_all_matches(db=db, match_id=match_id)
+def get_match_history(user_id: UUID, db: Session = Depends(get_db)):
+    matches = match_service.get_all_matches(db=db, user_id=user_id)
     if not matches:
         raise HTTPException(status_code=404, detail="No match history found")
     return matches
--- backend/src/app/schemas/problem.py
+++ backend/src/app/schemas/problem.py
@@ -3,18 +3,18 @@ from typing import Annotated
 from pydantic import AnyHttpUrl, BaseModel, StringConstraints, conlist, constr, EmailStr, Field
 from typing import Literal
 
-Difficulty = Literal["easy", "medium", "hard"]
+Difficulty = Literal["Easy", "Medium", "Hard"]
 class Problem(BaseModel):
     id: int
     problem_id: int
     problem_link: AnyHttpUrl
     methods_video_link: AnyHttpUrl
     categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9_]+$", max_length=32)], min_length=1, max_length=5)
-    difficulty: list[Difficulty]
+    difficulty: Difficulty
 
 class ProblemCreate(BaseModel):
     problem_id: int
     problem_link: AnyHttpUrl
     methods_video_link: AnyHttpUrl
     categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9_]+$", max_length=32)], min_length=1, max_length=5)
-    difficulty: list[Difficulty]
+    difficulty: Difficulty
