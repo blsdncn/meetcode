26a46672c6bc653e1ac0a91f34b96eb278e54ad7
jienzheng <jienzheng02@gmail.com>
2025-05-06 03:10:15 -0700

    Changed github variable for OAuth

--- frontend/src/lib/auth.tsx
+++ frontend/src/lib/auth.tsx
@@ -73,7 +73,7 @@ export const authOptions: NextAuthOptions = {
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_SECRET_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
