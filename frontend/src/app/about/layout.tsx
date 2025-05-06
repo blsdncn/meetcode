75a2bece74bbf9347e605e9517bd02c0b3d0c8a0
oyasumiku <98620862+oyasumiku@users.noreply.github.com>
2025-05-05 21:57:04 -0700

    Fixed

--- frontend/src/app/about/layout.tsx
+++ frontend/src/app/about/layout.tsx
@@ -11,11 +11,11 @@ export default function SupportLayout({
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<p className="w-full text-white-800 text-base">
-				Have questions?{' '}
+				
 				<Link href="/about/contact" className="text-blue-600 underline hover:text-blue-800">
-					Contact us here
+					
 				</Link>
-				.
+				
 			</p>
 			{children}
 		</div>
