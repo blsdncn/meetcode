f00d66bc618f49d5e9e70aeffb4795fabb96e057
teetruc <ttruc7290@sdsu.edu>
2025-05-06 20:19:27 -0700

    Changed hyperlink to contact page for forget password

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -128,7 +128,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
                     <a
-                      href="/forgot-password"
+                      href="/about/contact"
                       className="ml-auto text-sm underline-offset-4 hover:underline"
                     >
                       Forgot your password?
