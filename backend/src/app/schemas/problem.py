9aba4df9771a0c24aac3e34f8aae79c13c896063
jienzheng <jienzheng02@gmail.com>
2025-05-07 08:01:01 -0700

    Matchmaking page a little buggy, but everything works

--- backend/src/app/schemas/problem.py
+++ backend/src/app/schemas/problem.py
@@ -1,16 +1,16 @@
-from pydantic import BaseModel, conlist, AnyHttpUrl
+from datetime import datetime
 from typing import Annotated
-from pydantic import StringConstraints
+from pydantic import AnyHttpUrl, BaseModel, StringConstraints, conlist, constr, EmailStr, Field
 from typing import Literal
 
 Difficulty = Literal["Easy", "Medium", "Hard"]
 
-class Problem(BaseModel):
+class ProblemResponse(BaseModel):
     id: int
     problem_id: int
     problem_link: AnyHttpUrl
     methods_video_link: AnyHttpUrl
-    categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9_]+$", max_length=32)], min_length=1, max_length=5)
+    categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9-\s()]+$", max_length=32)], min_length=0, max_length=8)
     difficulty: Difficulty
 
     class Config:
@@ -20,5 +20,27 @@ class ProblemCreate(BaseModel):
     problem_id: int
     problem_link: AnyHttpUrl
     methods_video_link: AnyHttpUrl
-    categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9_]+$", max_length=32)], min_length=1, max_length=5)
+    categories: conlist(Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9-\s()]+$", max_length=32)], min_length=0, max_length=8)
     difficulty: Difficulty
+
+    class Config:
+        from_attributes = True
+    # class Config:
+    #     json_schema_extra = {
+    #         "example": {
+    #             "problem_id": 1,
+    #             "problem_link": "https://leetcode.com/problems/two-sum/description/",
+    #             "methods_video_link": "https://www.youtube.com/watch?v=KLlXCFG5TnA",
+    #             "categories": ["Array", "HashTable"]
+    #         }
+    #     }
+
+class UniqueTagsResponse(BaseModel):
+    tags: list[Annotated[str, StringConstraints(pattern=r"^[a-zA-Z0-9-\s()]+$", max_length=64)]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "tags": ["array", "of", "tags"]
+            }
+        }
